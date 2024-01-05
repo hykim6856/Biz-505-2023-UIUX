bae04ae10664a3c2e66fec316ba62c6171d5e5c5
hykim6856 <hykim6856@naver.com>
2024-01-05 12:47:33 +0900

    js수정

--- UI_005/js/image2.js
+++ UI_005/js/image2.js
@@ -2,7 +2,7 @@ const IMG_TAG_NAME = "IMG";
 const ALT_TEXT = {
   인증사진: "인증사진",
   바라보기: "바라보기",
-  스피츠: "스피츠 종",
+  스피츠: "스피츠",
   눈치보기: "눈치보기",
   놀아주기: "놀아주기",
   첫만남: "첫만남",
@@ -16,8 +16,16 @@ const img_container_click_handler = (event) => {
     // alert("클릭" + tag_name + alt_text);
     if (alt_text === ALT_TEXT.인증사진) {
       importHTML(img_container, "./01.html");
+    } else if (alt_text === ALT_TEXT.스피츠) {
+      importHTML(img_container, "./02.html");
     } else if (alt_text === ALT_TEXT.바라보기) {
       importHTML(img_container, "./03.html");
+    } else if (alt_text === ALT_TEXT.눈치보기) {
+      importHTML(img_container, "./04.html");
+    } else if (alt_text === ALT_TEXT.놀아주기) {
+      importHTML(img_container, "./05.html");
+    } else if (alt_text === ALT_TEXT.첫만남) {
+      importHTML(img_container, "./06.html");
     }
   }
 };
